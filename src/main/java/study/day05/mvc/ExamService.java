cf7e8c1e9e6cf5ff8ab792de5201f702ab90cc01
lth0330 <dlxogud555@gmail.com>
2026-03-04 15:17:45 +0900

    2026-03-04(수정)@transactional 추가

--- src/main/java/study/day05/mvc/ExamService.java
+++ src/main/java/study/day05/mvc/ExamService.java
@@ -1,5 +1,6 @@
 package study.day05.mvc;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -53,6 +54,7 @@ public class ExamService {
     }
 
     // U : 수정 update
+    @Transactional
     public boolean 수정(ExamDto examDto){
         // update 대신에 JPA 영속성 사용한다.
         // 데이터베이스와 자바객체 연결되는 상태를 계속적으로 유지
@@ -60,7 +62,7 @@ public class ExamService {
         // 1] 수정할 entity 찾기 , PK
          Optional< ExamEntity > optional =
                  examRepository.findById(examDto.getEno());   //findById(수정할 pK 번호);
-        // 2] 만약에 entitiy가 존자하면 , . isPresent() : 조회 결과가 있으면 true , 없으면 false 반환 함수
+        // 2] 만약에 entity가 존재하면 , . isPresent() : 조회 결과가 있으면 true , 없으면 false 반환 함수
         if(optional.isPresent()){
             ExamEntity examEntity = optional.get(); //존재한 앤티티 꺼내기
             // ************************ 영속성 이용한 수정 *************************//
@@ -70,3 +72,34 @@ public class ExamService {
     return false;
     }
 }
+/*
+    1] < > : 제네틱타입, 객체 생성할때 타입 지정
+    2] Optional < > : 객체내 null값 제어 기능/함수 제공하는 클래스,  null에 따른 안전한 객체 사용
+        1. .isPresent() : 만약에 null 이면 false 반환, 아니면 true 반환
+        2. .get() : 객체 반환
+        3. .orElse( null일때 값 )
+        4. .orElseThrow( 예외 값 )
+    사용처 : JPA에서 findById() 반환 타입  그외 몇몇 라이브러리에 사용된다.
+    사용법 :
+        1] Optional<앤티티> 변수명 = repository.findById( )
+        2] 앤티티 변수명 = repository.findById().orElse
+    3] JPA 기본 제공
+        1. .findAll()                : 모든 레코드/객체/앤티티 조회, 반환티입 List<앤티티명>
+        2. .findById(조회할 번호)      : 특정 Pk 번호의 앤티티 반환,  반환 타입 : Optional<앤티티명>
+        3. .save(저장할 앤티티)        : 특정 앤티티를 저장(insert). 반환티입 : 앤티티
+        4. .deleteById (삭제할 pk번호) : 특정 pk번호의 앤티티 삭제(delete), 반환 티입 : void
+        5. 수정함수는 존재하지 않는다. <영속성 특징>
+            - 영속성 갖는 시점 : save, findAll, findById 등등 반환된 앤티티가 영속된 앤티티
+            * 영속성이란? 데이터베이스와 자바객체 연결하는 관계
+            - 영속된 앤티티를 .setter 이용하여 객체 수정하면 자동으로 데이터베이스도 반영된다.
+            - @Transactional 갖는 클래스/매소드는 여러 SQL들을 하나의 묶음으로 한 번에 처리
+                - 즉] 트랜잭션이란? 여러 SQL들을 묶에서 하나라도 실패하면 모두 실패(Rollback), 모두 성공이면 최종성공(commit)
+                - 예1] 이체 기능은 [1] 출금 [2] 입금  2개 이상의 기능을 묶음 기능
+                    - 출금과 입금 중에 하나라도 문제가 발생하면 (이체)전체 취소
+                 영속된 객체를 .setter 이용하여 여러개 수정함으로써 여러개 수정(update)들을 하나로 처리
+                    -즉]수정하면 함수는 @Transactional가 필수이다.
+
+                - 예2] 주문 기능은 [1] 장바구니 취소 [2] 입금상태확인 [3] 재고 차감 [4] 택배등록
+                    하나라도 잘못되면 전체 취소
+
+ */

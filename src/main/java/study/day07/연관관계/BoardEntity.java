6fd4f3895778dbe775b11b3d1387a66cd5632d85
lth0330 <dlxogud555@gmail.com>
2026-03-09 11:20:00 +0900

    2026-03-09(단방향, 양방향 참조)앤티티

---
+++ src/main/java/study/day07/연관관계/BoardEntity.java
@@ -0,0 +1,26 @@
+package study.day07.연관관계;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "board")
+public class BoardEntity {
+    // ** 단방향 **  FK 만들기 **
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer bno;
+    private String bcontent;
+
+    @ManyToOne      // 다수가 하나에게 . M:1  여러개 게시물이 하나의 카테고리에게 참조
+    @JoinColumn(name = "cno")   // 관례적으로 fk 필드명도 pk 필드명과 동일
+    private CategoryEntity categoryEntity;
+}

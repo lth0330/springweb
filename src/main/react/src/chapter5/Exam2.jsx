88a0d88befba7e3cc929074095d57eb1a8e77f3d
lth0330 <dlxogud555@gmail.com>
2026-03-10 17:28:39 +0900

    2026-03-010(Exam2 onClick)

---
+++ src/main/react/src/chapter5/Exam2.jsx
@@ -0,0 +1,45 @@
+
+function FrontComp(){
+  // { /*   */ } 중괄호 안에 /* */ 쓰기
+  return(<>
+      {/* JSX(return에서 사용되는 (xml)문법)에서 주석처리하는 방법*/}
+      <li>
+        <a href="/" onClick={ ( ) => { props.onMyEvent1() }} >프론트엔드</a>
+      </li>
+      <ul>
+        <li>HTML</li>
+        <li>CSS3</li>
+        <li>자바스크립트</li>
+        <li>JQuery</li>
+      </ul>
+  
+  </>)
+}
+
+function Exam2(){
+
+  function 출력함수(){alert("출력된 메세지1");} // 선언적 함수
+
+  // 익명함수는 이름이 없는 함수(재사용이 안된다. 일회성 또는 이벤트 함수로 사용 )
+  return(<>
+      <h3>이벤트 처리 p.100</h3>
+      <button onClick={ 출력함수 } > 리액트 버튼</button>
+      <button onClick={function(){alert('출력된 메세지2');}}>리액트 버튼2(익명함수)</button>
+      <button onClick={()=> {alert("출력되는 메세지3")}}>리액트 버튼3(화살표 함수)</button>
+      <FrontComp onMyEvent1 = { ( )=>{alert("프론트엔드 클릭됨")}}></FrontComp>
+  
+  </>)
+}
+
+export default Exam2;
+/*
+  리액트 이벤트에서 주의할점
+  1. OnClick --> onClick , 합성이벤트(리액트가 핸들러/연결을 통해 이벤트 실행)
+  2. OnClick에서 함수를 실행하는 구조가 아니고 *전달*하는 구조
+      1] 선언방법 : function 함수명(){}
+      2] 호출방법 : 함수명()
+      3] 함수란? 여러개 계산식/코드 (데이터x) 묶음 
+      4] 변수란? 하나의 값을 저장하는 공간
+    ** HTML : <button onclick="출력함수()">
+    ** REACT :<button onClick={ 출력함수 } > 
+*/
--- src/main/react/src/main.jsx
+++ src/main/react/src/main.jsx
@@ -6,6 +6,12 @@ import App from './App.jsx'
 // 기존코드
 //createRoot(document.getElementById('root')).render(<App />)
 
+/*
 // chapter 4 예제
 import Exam1 from './chapter4/Exma1.jsx'; // 컴포넌트 불러오기
 createRoot(document.querySelector("#root")).render(<Exam1/>);
+*/
+
+//chapter 5 예제
+import Exam2 from './chapter5/Exam2.jsx' // 컴포넌트 불러오기
+createRoot(document.querySelector("#root")).render(<Exam2/>);

5d1ba0e796edcdd18ddbdd63e090821ea96821e6
lth0330 <dlxogud555@gmail.com>
2026-03-24 14:10:27 +0900

    2026-03-24(토큰 생성 및 값추출)

--- src/main/java/study/day15/JWTController.java
+++ src/main/java/study/day15/JWTController.java
@@ -15,9 +15,16 @@ public class JWTController {
 
     private final JWTService jwtService;
 
-    @GetMapping("/create")
+
+    @GetMapping("/create")  // http://localhost:8080/api/jwt/create?data=%EB%B0%94%EB%82%98%EB%82%98
     public ResponseEntity<?> 토큰생성(@RequestParam String data){
         return ResponseEntity.ok(jwtService.토큰생성(data));
+    }
+
+    // [2] JWT 토큰 값 추출 == 암호화된 JWT 토큰을 다시 평문으로
+    @GetMapping("/read")
+    public ResponseEntity<?> 토큰값추출(@RequestParam String 토큰){
+        return ResponseEntity.ok(jwtService.토큰값추출(토큰));
     }
 
 }
--- src/main/java/study/day15/JWTService.java
+++ src/main/java/study/day15/JWTService.java
@@ -1,6 +1,7 @@
 package study.day15;
 
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -17,23 +18,41 @@ import java.util.Date;
 @RequiredArgsConstructor
 public class JWTService {
 
+    // [1]
     // * 내가 만든 임의의 값으로 토큰에 사용되는 암호화 계산식의 비밀번호
     private final String 비밀번호 = "123456789123456789123456789123456789";
 
     // * 내가 만든 임의의 값(비밀번호) 해시값으로 반환
     private final Key 비밀키 = Keys.hmacShaKeyFor(비밀번호.getBytes());
+
     // jwt 토큰 생성
-    public String 토큰생성(String data){
+    public String 토큰생성(String data) {
         String token = Jwts.builder() // 토튼 생성 시작
                 //.claim("key",value) 토큰에 저장할 자료를 key와 value 대입하낟
-                .claim("data",data)
+                .claim("data", data)
                 .setIssuedAt(new Date())    // 토튼 발급날짜/시간 , new Date() : 시스템날짜/시간반환
-                .setExpiration(new Date(System.currentTimeMillis()+1000*15))    // 토튼 유지/ 유효 시간, 초단위
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 15))    // 토튼 유지/ 유효 시간, 초단위
                 .signWith(비밀키, SignatureAlgorithm.HS256)//.signWith("비밀키",암호화알고리즘), 최종 토큰 암호화는 HS256 알고리즘 적용
                 .compact(); // [e] 토큰 최종 문자열로 반환
         return token;
     }
 
+    // [2] 값 추출
+    public String 토큰값추출(String 토큰) {
+        try {
+
+
+            Claims claims = Jwts.parser()   //  파싱 / 가져온다 뜻
+                    .setSigningKey(비밀키) // 서명 검증에 필요한 비밀키 대입
+                    .build()              // 비밀키 확인
+                    .parseClaimsJws(토큰)  // 검증할 토큰 대입한다.
+                    .getBody();           // 검증 성공시 클레임(내용물)을 가져온다
+            return (String) claims.get("data");    // 저장된 값은 무조건 Object 타입이다.
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
 }
 /*
     JWT (json web token)

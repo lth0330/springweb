4a57d31f47f1a300e780ba8913d054310c7c4db0
lth0330 <dlxogud555@gmail.com>
2026-03-31 14:55:20 +0900

    2026-03-31(라우터 훅) - 쿼르스트링 번호 가져오기

--- src/main/react/src/chapter11/Exam2.jsx
+++ src/main/react/src/chapter11/Exam2.jsx
@@ -4,21 +4,27 @@ import Home from "./components/Home";
 import NotFound from "./components/NotFound";
 import CommonLayout from "./components/CommonLayout";
 import LayoutIndex from "./components/LayoutIndex";
+import RouterHooks from "./components/RputerHooks";
 
 // Exam2.jsx
-export default function Exam2(props){
+export default function Exam2(props) {
 
-  return(<>
+  return (<>
     <TopNavi> </TopNavi>
     <Routes>{/*라우터들 설정 시작*/}
-      <Route path="/" element={<Home/>} />    {/*라우터 설정 */}
-    
-      <Route path="/intro" element={<CommonLayout/>}>
-        <Route index element ={<LayoutIndex/>} />
+      <Route path="/" element={<Home />} />    {/*라우터 설정 */}
+
+      <Route path="/intro" element={<CommonLayout />}>
+
+        {/* 경로가 별도로 없을때 index 지정*/}
+        <Route index element={<LayoutIndex />} />
+
+        {/* 쿼리스트링을 갖는 URL */}
+        <Route path="router" element={<RouterHooks/>}/>
+
       </Route>
+      <Route path="*" element={<NotFound />} /> {/*라우터 설정 */}
 
-      <Route path="*" element={<NotFound/>}/> {/*라우터 설정 */}
-
-      </Routes>
+    </Routes>
   </>)
 }
--- src/main/react/src/chapter11/components/RputerHooks.jsx
+++ src/main/react/src/chapter11/components/RputerHooks.jsx
@@ -1,17 +1,44 @@
+import { useLocation, useSearchParams } from "react-router-dom"
 
-export default function RouterHooks(props){
-  return(<>
+export default function RouterHooks( props ){
+    
+    const location = useLocation();
 
-    // [1] useSearchParams 훅 선언한다.   이유 : URL 상의 쿼리스트링 값 가져오기
-    const = [searchParams , setSearchParams] = useSeachParams();
-    const mode = searchParams.get('mode'); // 쿼리스트링내 mode 변수명 값 가져오기
-    const pageNum = searchParams.get('pageNum') // 쿼리스트링내 pageNum변수명 값 가져오기
+    // [1] useSearchParams 훅 선언한다.  이유 : URL 상의 쿼리스트링 값 가져오기 
+    const [ searchParams , setSearchParams ] = useSearchParams();
+    const mode = searchParams.get( 'mode' ); // 쿼리스트링내 mode변수명 값 가져오기 
+    const pageNum = searchParams.get( 'pageNum'); // 쿼리스트링내 pageNum변수명 값 가져오기
 
-    // [2] changeMode,  만약에 mode가 list 이면 view 변경 아니면 list 로 변경 
-    const changeMode = ( ) => {
-        const changeMode = ( mode =="list") ? "view" : "list" ;
-        setSearchParams({mode : nextMode, pageNum : pageNum})
+    // [2] changeMode, 만약에 mode가 list 이면 view 변경 아니면 list 로 변경 
+    const changeMode = ( )=>{
+        const nextMode = ( mode == 'list') ? 'view' : 'list' ; 
+        setSearchParams( { mode : nextMode , pageNum : pageNum } )
     }
 
-  </>)
+    // [3] nextPage , 만약에 pageNum가 null 이면 1페이지 아니면 + 1
+    const nextPage = ( )=>{
+        let pageTemp = (pageNum == null || isNaN( pageNum )) ? 1 : parseInt( pageNum) + 1;
+        setSearchParams( {mode : mode , pageNum : pageTemp } )
+    }
+
+    // [4] prevPage , 만약에 pageNum가 null 이면 1페이지 아니면 - 1
+    const prevPage = ( )=>{
+        let pageTemp = (pageNum == null || isNaN( pageNum )) ? 1 : parseInt( pageNum) - 1;
+        setSearchParams( {mode : mode , pageNum : pageTemp } )
+    }
+
+    return (<>
+        <h2> 라우터 관련 Hook </h2>
+        <div>
+            <ul>
+                <li> URL : { location.pathname } </li>
+                <li> 쿼리스트링 : { location.search } </li>
+                <li> mode : { mode } </li>
+                <li> pageNum : { pageNum } </li>
+            </ul>
+            <button onClick={ changeMode}> mode 변경 </button>
+            <button onClick={ prevPage }> 이전 페이지 </button>
+            <button onClick={ nextPage }> 다음 페이지 </button>
+        </div>
+    </>)
 }
